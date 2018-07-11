ecdc3f3f3b62611b48dda6a508f94ee01136a5ba
Sam Raby <Sam@Sams-MacBook-Pro-4.local>
2018-07-11 13:03:25 -0400

    added legend and updated infowindow

--- map.js
+++ map.js
@@ -1,44 +1,49 @@
 
 $(document).ready(function () {
-                cartodb.createVis('map', 'http://uducla.carto.com/u/uducla/api/v2/viz/664b2ba5-b804-4fd8-b734-c813d78242de/viz.json', {
-                        shareable: false, 
-                        search: true,
-                        infowindow: true,
-                        tooltip: true,
-                        loaderControl: false,
-                        layer_selector: true,
-                        detectRetina: true,
-                        center_lat: 33.936454,
-                        center_lon: -118.232699,
-                        zoom: 10
-                    }).on('done', function(vis,layers) {
-                        // Get the "native" Leaflet map and add a zoom control to it 
-                        var map = vis.getNativeMap();
+    cartodb.createVis('map', 'http://uducla.carto.com/u/uducla/api/v2/viz/664b2ba5-b804-4fd8-b734-c813d78242de/viz.json', {
+            shareable: false, 
+            search: true,
+            infowindow: true,
+            tooltip: true,
+            loaderControl: false,
+            layer_selector: true,
+            detectRetina: true,
+            center_lat: 33.936454,
+            center_lon: -118.232699,
+            zoom: 10
+        }).on('done', function(vis,layers) {
+            // Get the "native" Leaflet map
+            var map = vis.getNativeMap();
 
-                        map.addControl(L.control.zoom());
+            // Add a zoom control to it 
+            map.addControl(L.control.zoom());
 
-                        var sublayer = layers[1].getSubLayer(1);
-                        sublayer.setInteraction(true);
-                        sublayer.setInteractivity('cartodb_id,name,textshort,textlong,lat,lng,photo1cap,photo1url,photo2cap,photo2url,photo3cap,photo3url,videocap,videourl,videoframe,audiocap,audiourl');
+            // Set infowindow
+            var sublayer = layers[1].getSubLayer(1);
+            sublayer.setInteraction(true);
+            sublayer.setInteractivity('cartodb_id,name,textshort,textlong,lat,lng,photo1cap,photo1url,photo2cap,photo2url,photo3cap,photo3url,videocap,videourl,videoframe,audiocap,audiourl');
 
-                        //sublayer.infowindow.set('template_type', 'mustache');
-                        sublayer.infowindow.set('sanitizeTemplate',false);
-                        sublayer.infowindow.set('template', $('#infowindow_template').html());
+            sublayer.infowindow.set('sanitizeTemplate',false);
+            sublayer.infowindow.set('template', $('#infowindow_template').html());
 
-                        
-                        vis.addOverlay({
-                              type: 'tooltip',
-                              layer: sublayer,
-                              template: $('#hover_template').html(),
-                              position: 'top|right',
-                              fields: [{name: 'name', textshort: 'textshort'}]
-                            }); 
 
-                        sublayer.on('featureClick', function(e, latlng, pos, data, layerNumber) {
-                            map.panTo(latlng);
-                        })}).on('error', function() {
-                    console.log("some error occurred");
-                });
+            // Set tooltip
+            vis.addOverlay({
+                  type: 'tooltip',
+                  layer: sublayer,
+                  template: $('#hover_template').html(),
+                  position: 'top|right',
+                  fields: [{name: 'name', textshort: 'textshort'}]
+                }); 
+
+            // Set map pan on point click
+            sublayer.on('featureClick', function(e, latlng, pos, data, layerNumber) {
+                map.panTo(latlng);
+            })
+
+        }).on('error', function() {
+            console.log("some error occurred");
+    });
 });
 
 
